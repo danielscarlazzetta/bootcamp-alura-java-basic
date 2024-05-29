d85fb84f6bf461f785ccce6ee600c8111e2d3067
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-05-28 21:01:46 -0400

    creacion de loop

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Loop {
+
+    public static void main(String[] args) {
+         Scanner teclado = new Scanner(System.in);
+
+         double nota = 0;
+         double promedionotas = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Ingrese el promedio de notas: ");
+            nota = teclado.nextDouble();
+            promedionotas =+ promedionotas + nota;
+        }
+
+        System.out.println(" la media de notas es: " + promedionotas/3);
+    }
 }
